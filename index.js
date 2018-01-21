86d4b0f4926f408480195a9ef546fb9c2a2e7242
craig stevens <c_stevens44@hotmail.com>
2018-01-21 21:42:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = "I don't do much."
 kittens = ["Milo", "Otis", "Garfield"]
 
 var kittens = ["Milo", "Otis", "Garfield"]
-function destructivelyAppendKitten(kittens, cat) {
+function destructivelyAppendKitten(kittens) {
 kittens.push("Ralph");
 return kittens
 }
