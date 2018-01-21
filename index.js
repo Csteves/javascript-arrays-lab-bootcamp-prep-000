78699152c4758fb9a926bb7881675de448d325f1
craig stevens <c_stevens44@hotmail.com>
2018-01-21 21:32:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+kittens = ["Milo", "Otis", "Garfield"]
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(kittens, cat) {
+  var kittens = ["Milo", "Otis", "Garfield"]
+var cat = kittens.push("ralph")
+return cat
+}
